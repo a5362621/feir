20e791adb201082010ab74e3549ae02112d4f0b7
Xin.min <244970635@qq.com>
2018-05-25 15:47:39 +0800

    DEVELOP: first time git push for my lover

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+//$i="I'm responsible for fall in love with HeYunFeir. It's serious";
+//$j=wordwrap($i,10,"<br>\n",true);
+//echo $j;
+
+$ajaxSends=$_GET;
+//$ajaxSends=$_POST;
+echo json_encode($ajaxSends);
---
+++ 开启brower-sync.bat
@@ -0,0 +1,4 @@
+echo 正在启动browser-sync，请稍等......
+browser-sync start --server  --files "*.html, css/*.css, js/*.js"
+echo browser-sync已启动
+echo. & pause
